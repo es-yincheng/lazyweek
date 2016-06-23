00373f4e5f591ec74b99c3a460176e80990266dc
尹成 <yc@yinchengdeimac.local>
2016-06-23 14:25:55 +0800

    Initial Commit

---
+++ lazyweek/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  lazyweek
+//
+//  Created by 尹成 on 16/6/23.
+//  Copyright © 2016年 尹成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ lazyweek/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  lazyweek
+//
+//  Created by 尹成 on 16/6/23.
+//  Copyright © 2016年 尹成. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
